7ca9ac4ece360c7b82f4ec0f240d03d34035f7cf
dexterski007 <mbouj007@gmail.com>
2024-09-28 19:55:37 +0100

    new fix

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -11,7 +11,7 @@ class AppController {
   static async getStats(req, res) {
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
-    res.status(200).json({ users: `${users}`, files: `${files}` });
+    res.status(200).json({ users: users, files: files });
   }
 }
 
