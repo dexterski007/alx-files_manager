fdd96ee401917fd542fd661a2361d586c0502f35
dexterski007 <mbouj007@gmail.com>
2024-09-28 13:35:44 +0100

    task1

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+  isAlive() {
+    return this.client.connected;
+  }
+  async get(key) {
+    const result = await this.getAsync(key);
+    return result;
+  }
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
