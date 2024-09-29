25285e9a005d077512c6489480b4783acecd3737
dexterski007 <mbouj007@gmail.com>
2024-09-29 20:51:28 +0100

    fix

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,87 +2,110 @@ import { v4 as uuidv4 } from 'uuid';
 import { ObjectId } from 'mongodb';
 import { mkdir, writeFile } from 'fs';
 import Queue from 'bull';
-import path from 'path';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class FilesController {
   static async postUpload(req, res) {
     const fileQ = new Queue('fileQ');
-    const validTypes = ['folder', 'file', 'image'];
-    const {
-      name,
-      type,
-      parentId = '0',
-      isPublic = false,
-      data,
-    } = req.body;
-    if (!name) return res.status(400).json({ error: 'Missing name' });
-    if (!type || !validTypes.includes(type)) return res.status(400).json({ error: 'Missing type' });
-    if (!data && type !== 'folder') return res.status(400).json({ error: 'Missing data' });
-    const token = req.header('X-Token');
-    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+    const dir = process.env.FOLDER_PATH || '/tmp/files_manager';
 
-    const userId = await redisClient.get(`auth_${token}`);
-    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+    async function getIdKey(req) {
+      const userInfo = { userId: null, key: null };
 
-    let parentFile = null;
-    if (parentId !== '0') {
-      try {
-        parentFile = await dbClient.dbClient.collection('files').findOne({ _id: ObjectId(parentId) });
-        if (!parentFile) return res.status(400).json({ error: 'Parent not found' });
-        if (parentFile.type !== 'folder') return res.status(400).json({ error: 'Parent is not a folder' });
-      } catch (err) {
-        console.error(err);
-        return res.status(500).send();
-      }
+      const token = req.header('X-Token');
+      if (!token) return userInfo;
+
+      userInfo.key = `auth_${token}`;
+      userInfo.userId = await redisClient.get(userInfo.key);
+
+      return userInfo;
     }
 
-    const fileDocument = {
-      userId,
-      name,
-      type,
-      isPublic,
+    const { userId } = await getIdKey(req);
+
+    function isValidUser(id) {
+      try {
+        ObjectId(id);
+      } catch (error) {
+        return false;
+      }
+      return true;
+    }
+
+    if (!isValidUser(userId)) return res.status(401).json({ error: 'Unauthorized' });
+
+    const user = await dbClient.dbClient.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) return res.status(401).json({ error: 'Unauthorized' });
+
+    const fileName = req.body.name;
+    if (!fileName) return res.status(400).json({ error: 'Missing name' });
+
+    const fileType = req.body.type;
+    if (!fileType || !['folder', 'file', 'image'].includes(fileType)) return res.status(400).json({ error: 'Missing type' });
+
+    const fileData = req.body.data;
+    if (!fileData && fileType !== 'folder') return res.status(400).json({ error: 'Missing data' });
+
+    const publicFile = req.body.isPublic || false;
+    let parentId = req.body.parentId || 0;
+    parentId = parentId === '0' ? 0 : parentId;
+    if (parentId !== 0) {
+      const parentFile = await dbClient.dbClient.collection('files').findOne({ _id: ObjectId(parentId) });
+      if (!parentFile) return res.status(400).json({ error: 'Parent not found' });
+      if (parentFile.type !== 'folder') return res.status(400).json({ error: 'Parent is not a folder' });
+    }
+
+    const fileInsertData = {
+      userId: user._id,
+      name: fileName,
+      type: fileType,
+      isPublic: publicFile,
       parentId,
     };
 
-    if (type === 'folder') {
-      await dbClient.dbClient.collection('files').insertOne(fileDocument);
-      return res.status(201).send({
-        id: fileDocument._id,
-        userId: fileDocument.userId,
-        name: fileDocument.name,
-        type: fileDocument.type,
-        isPublic: fileDocument.isPublic,
-        parentId: fileDocument.parentId,
+    if (fileType === 'folder') {
+      await dbClient.dbClient.collection('files').insertOne(fileInsertData);
+      return res.status(201).json({
+        id: fileInsertData._id,
+        userId: fileInsertData.userId,
+        name: fileInsertData.name,
+        type: fileInsertData.type,
+        isPublic: fileInsertData.isPublic,
+        parentId: fileInsertData.parentId,
       });
     }
 
-    const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-    const localPath = path.join(folderPath, uuidv4());
-    const fileData = Buffer.from(data, 'base64');
-    mkdir(folderPath, { recursive: true }, (err) => {
-      if (err) return res.status(400).json({ error: err.message });
+    const fileUid = uuidv4();
+
+    const decData = Buffer.from(fileData, 'base64');
+    const filePath = `${dir}/${fileUid}`;
+
+    mkdir(dir, { recursive: true }, (error) => {
+      if (error) return res.status(400).json({ error: error.message });
       return true;
     });
-    writeFile(localPath, fileData, (err) => {
-      if (err) return res.status(400).json({ error: err.message });
+
+    writeFile(filePath, decData, (error) => {
+      if (error) return res.status(400).json({ error: error.message });
       return true;
     });
-    fileDocument.localPath = localPath;
-    await dbClient.dbClient.collection('files').insertOne(fileDocument);
+
+    fileInsertData.localPath = filePath;
+    await dbClient.dbClient.collection('files').insertOne(fileInsertData);
 
     fileQ.add({
-      userId: fileDocument.userId,
-      fileId: fileDocument._id,
+      userId: fileInsertData.userId,
+      fileId: fileInsertData._id,
     });
+
     return res.status(201).json({
-      id: fileDocument._id,
-      userId: fileDocument.userId,
-      name: fileDocument.name,
-      type: fileDocument.type,
-      isPublic: fileDocument.isPublic,
-      parentId: fileDocument.parentId,
+      id: fileInsertData._id,
+      userId: fileInsertData.userId,
+      name: fileInsertData.name,
+      type: fileInsertData.type,
+      isPublic: fileInsertData.isPublic,
+      parentId: fileInsertData.parentId,
     });
   }
 }
